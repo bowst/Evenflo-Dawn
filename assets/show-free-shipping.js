2d8c59a59a381849d87b1d6a26861bf3aad188bc
Kristen Legacy <kristenlegacy@gmail.com>
2025-09-24 17:57:57 -0400

    Free shipping value check update (its correct on live but git seems to still have the wrong value)

--- assets/show-free-shipping.js
+++ assets/show-free-shipping.js
@@ -16,7 +16,7 @@ function showFreeShipping(){
 
 		productPrice = productPrice.replace("$", "");
 		
-		if(productPrice > 49.00 ){
+		if(productPrice > 150.00 ){
 			shippingNotice.classList.remove('visually-hidden');
 			shippingNoticeFooter.classList.remove('hidden');
 			//console.log('free');
